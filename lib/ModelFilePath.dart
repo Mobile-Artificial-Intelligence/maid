fda33284b6ea6aa86eeb4fd362d414fc52827e50
Thibaut Léaux <thibaut@bip-rep.com>
2023-03-28 12:46:44 +0200

    saving file path if file found

--- lib/ModelFilePath.dart
+++ lib/ModelFilePath.dart
@@ -7,42 +7,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ModelFilePath {
 
-  // static Future<Directory?> getDownloadPath() async {
-  //   Directory? directory;
-  //   try {
-  //     //ask for permission
-  //     if (Platform.isAndroid) {
-  //       await Permission.storage.request();
-  //       var status = await Permission.storage.request();
-  //       if (!status.isGranted) {
-  //         print("Permission denied : $status");
-  //         return null;
-  //         // We didn't ask for permission yet or the permission has been denied before but not permanently.
-  //       }
-  //     }
-  //     if (Platform.isIOS) {
-  //       directory = await getApplicationDocumentsDirectory();
-  //       var newFolder = Directory('${directory.path}/Download');
-  //       if (newFolder.existsSync() == false) {
-  //         newFolder.createSync();
-  //       }
-  //       directory = newFolder;
-  //     } else {
-  //       directory = Directory('/storage/emulated/0/Download');
-  //       // Put file in global download folder, if for an unknown reason it didn't exist, we fallback
-  //       // ignore: avoid_slow_async_io
-  //       if (!await directory.exists()) {
-  //         directory = await getExternalStorageDirectory();
-  //       }
-  //     }
-  //   } catch (err, stack) {
-  //     print("Cannot get download folder path");
-  //   }
-  //   return directory;
-  // }
-
-
-
   static Future <String?>getFilePath() async {
 
     var prefs = await SharedPreferences.getInstance();
@@ -52,10 +16,12 @@ class ModelFilePath {
     }
 
     FilePickerResult? result = await FilePicker.platform.pickFiles(
+      withReadStream: true,
       type: FileType.custom,
       allowedExtensions: ['bin'],
     );
-    if (result != null) {
+    if (result?.files.single.path != null) {
+      prefs.setString('path', result!.files.single.path!);
       return result.files.single.path;
     } else {
       return null;
