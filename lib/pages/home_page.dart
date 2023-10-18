967c28c5b46027d4222a8542ce016721f047955d
dane madsen <dane_madsen@hotmail.com>
2023-10-18 19:10:30 +1000

    Fix issue with first character of response being truncated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,7 +40,7 @@ class _MaidHomePageState extends State<MaidHomePage> {
       chatWidgets.add(UserMessage(message: model.promptController.text.trim()));
       model.promptController.text += '\n${model.responseAliasController.text}';
       model.compilePrePrompt();
-      historyLength = model.promptController.text.trim().length + 3;
+      historyLength = model.promptController.text.trim().length + 2;
       historyLength += (responseLength == 0) ? model.prePrompt.length : 0;
       responseLength = 0;
       model.inProgress = true;
