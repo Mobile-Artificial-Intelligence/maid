0c13df78bf04aee638aa9d6660b525271ce7aea2
dane madsen <dane_madsen@hotmail.com>
2023-10-24 14:33:20 +1000

    Remove unused package

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,11 @@
 import FlutterMacOS
 import Foundation
 
-import llama
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  LlamaPlugin.register(with: registry.registrar(forPlugin: "LlamaPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
--- packages/llama/lib/llama.dart
+++
@@ -1,7 +0,0 @@
-import 'llama_platform_interface.dart';
-
-class Llama {
-  Future<String?> getPlatformVersion() {
-    return LlamaPlatform.instance.getPlatformVersion();
-  }
-}
