94d05e46ed0a2091cb4fa17ad63321f7bc516ccb
dane madsen <dane_madsen@hotmail.com>
2023-10-30 23:05:46 +1000

    Fix prompt not being passed on first prompt

--- lib/config/butler.dart
+++ lib/config/butler.dart
@@ -5,6 +5,8 @@ import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
 import 'package:maid/config/butler_bindings.dart';
+import 'package:maid/config/character.dart';
+import 'package:maid/config/model.dart';
 import 'package:maid/config/settings.dart';
 
 class Lib {
@@ -77,32 +79,32 @@ class Lib {
     _hasStarted = true;
     
     final params = calloc<butler_params>();
-    params.ref.model_path = settings.getCurrentModel().modelPath.toNativeUtf8().cast<Char>();
-    params.ref.preprompt = settings.getCurrentCharacter().getPrePrompt().toNativeUtf8().cast<Char>();
-    params.ref.input_prefix = settings.getCurrentCharacter().userAliasController.text.trim().toNativeUtf8().cast<Char>();
-    params.ref.input_suffix = settings.getCurrentCharacter().responseAliasController.text.trim().toNativeUtf8().cast<Char>();
-    params.ref.seed = settings.getCurrentModel().parameters["random_seed"] ? -1 : settings.getCurrentModel().parameters["seed"];
-    params.ref.n_ctx = settings.getCurrentModel().parameters["n_ctx"];
-    params.ref.n_threads = settings.getCurrentModel().parameters["n_threads"];
-    params.ref.n_batch = settings.getCurrentModel().parameters["n_batch"];
-    params.ref.n_predict = settings.getCurrentModel().parameters["n_predict"];
-    params.ref.instruct = settings.getCurrentModel().parameters["instruct"]       ? 1 : 0;
-    params.ref.memory_f16 = settings.getCurrentModel().parameters["memory_f16"]   ? 1 : 0;
-    params.ref.n_prev = settings.getCurrentModel().parameters["n_prev"];
-    params.ref.n_probs = settings.getCurrentModel().parameters["n_probs"];
-    params.ref.top_k = settings.getCurrentModel().parameters["top_k"];
-    params.ref.top_p = settings.getCurrentModel().parameters["top_p"];
-    params.ref.tfs_z = settings.getCurrentModel().parameters["tfs_z"];
-    params.ref.typical_p = settings.getCurrentModel().parameters["typical_p"];
-    params.ref.temp = settings.getCurrentModel().parameters["temperature"];
-    params.ref.penalty_last_n = settings.getCurrentModel().parameters["penalty_last_n"];
-    params.ref.penalty_repeat = settings.getCurrentModel().parameters["penalty_repeat"];
-    params.ref.penalty_freq = settings.getCurrentModel().parameters["penalty_freq"];
-    params.ref.penalty_present = settings.getCurrentModel().parameters["penalty_present"];
-    params.ref.mirostat = settings.getCurrentModel().parameters["mirostat"];
-    params.ref.mirostat_tau = settings.getCurrentModel().parameters["mirostat_tau"];
-    params.ref.mirostat_eta = settings.getCurrentModel().parameters["mirostat_eta"];
-    params.ref.penalize_nl = settings.getCurrentModel().parameters["penalize_nl"]     ? 1 : 0;
+    params.ref.model_path = model.parameters["model_path"].toString().toNativeUtf8().cast<Char>();
+    params.ref.preprompt = character.getPrePrompt().toNativeUtf8().cast<Char>();
+    params.ref.input_prefix = character.userAliasController.text.trim().toNativeUtf8().cast<Char>();
+    params.ref.input_suffix = character.responseAliasController.text.trim().toNativeUtf8().cast<Char>();
+    params.ref.seed = model.parameters["random_seed"] ? -1 : model.parameters["seed"];
+    params.ref.n_ctx = model.parameters["n_ctx"];
+    params.ref.n_threads = model.parameters["n_threads"];
+    params.ref.n_batch = model.parameters["n_batch"];
+    params.ref.n_predict = model.parameters["n_predict"];
+    params.ref.instruct = model.parameters["instruct"]       ? 1 : 0;
+    params.ref.memory_f16 = model.parameters["memory_f16"]   ? 1 : 0;
+    params.ref.n_prev = model.parameters["n_prev"];
+    params.ref.n_probs = model.parameters["n_probs"];
+    params.ref.top_k = model.parameters["top_k"];
+    params.ref.top_p = model.parameters["top_p"];
+    params.ref.tfs_z = model.parameters["tfs_z"];
+    params.ref.typical_p = model.parameters["typical_p"];
+    params.ref.temp = model.parameters["temperature"];
+    params.ref.penalty_last_n = model.parameters["penalty_last_n"];
+    params.ref.penalty_repeat = model.parameters["penalty_repeat"];
+    params.ref.penalty_freq = model.parameters["penalty_freq"];
+    params.ref.penalty_present = model.parameters["penalty_present"];
+    params.ref.mirostat = model.parameters["mirostat"];
+    params.ref.mirostat_tau = model.parameters["mirostat_tau"];
+    params.ref.mirostat_eta = model.parameters["mirostat_eta"];
+    params.ref.penalize_nl = model.parameters["penalize_nl"]     ? 1 : 0;
 
     _nativeLibrary.butler_start(params);
 
@@ -117,7 +119,7 @@ class Lib {
       } else if (data is SendPort) {
         completer.complete();
       } else if (data is int) {
-        settings.getCurrentModel().busy = false;
+        model.busy = false;
         maidOutput("");
       }
     });
@@ -125,7 +127,9 @@ class Lib {
   }
 
   void butlerContinue() {
-    String input = settings.getCurrentCharacter().promptController.text;
+    String input = character.promptController.text;
+    print("Input: $input");
+    Settings.log("Input: $input");
     Isolate.spawn(butlerContinueIsolate, {
       'input': input,
       'port': _sendPort
--- lib/config/settings.dart
+++ lib/config/settings.dart
@@ -10,22 +10,16 @@ Settings settings = Settings();
 class Settings { 
   Map<String, Model> _models = {};
   Map<String, Character> _characters = {};
-  String _currentModel = "default";
-  String _currentCharacter = "default";
   
   static String _logString = "";
 
   static String get getLog => _logString;
 
-  Settings() {
-    init();
-  }
-
   static void log(String message) {
     _logString += "$message\n";
   }
 
-  void init() async {
+  Future<void> init() async {
     var prefs = await SharedPreferences.getInstance();
     
     var modelsJson = json.decode(prefs.getString("models") ?? "{}");
@@ -53,31 +47,36 @@ class Settings {
       _characters["default"] = Character();
     }
 
-    _currentModel = prefs.getString("current_model") ?? "default";
-    _currentCharacter = prefs.getString("current_character") ?? "default";
+    model = _models[prefs.getString("current_model") ?? "default"] ?? Model();
+    character = _characters[prefs.getString("current_character") ?? "default"] ?? Character();
   }
 
   void save() async {
     var prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+
+    _models[model.getName()] = model;
+    _characters[character.getName()] = character;
+
     prefs.setString("models", json.encode(_models.map((key, value) => MapEntry(key, value.parameters))));
     prefs.setString("characters", json.encode(_characters.map((key, value) => MapEntry(key, value.toMap()))));
-    prefs.setString("current_model", _currentModel);
-    prefs.setString("current_character", _currentCharacter);
+    prefs.setString("current_model", model.getName());
+    prefs.setString("current_character", character.getName());
   }
 
-  Model getCurrentModel() {
-    if (_models[_currentModel] == null) {
-      _models[_currentModel] = Model();
-    }
-    
-    return _models[_currentModel]!;
+  List<String> getModels() {
+    return _models.keys.toList();
   }
 
-  Character getCurrentCharacter() {
-    if (_characters[_currentCharacter] == null) {
-      _characters[_currentCharacter] = Character();
-    }
-    
-    return _characters[_currentCharacter]!;
+  List<String> getCharacters() {
+    return _characters.keys.toList();
+  }
+
+  void setModel(String modelName) {
+    model = _models[modelName] ?? Model();
+  }
+
+  void setCharacter(String characterName) {
+    character = _characters[characterName] ?? Character();
   }
 }
