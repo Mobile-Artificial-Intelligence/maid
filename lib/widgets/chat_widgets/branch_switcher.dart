45cac855f74568a6976b60c29f46f6235d74d67f
dane madsen <dane_madsen@hotmail.com>
2023-11-07 09:20:46 +1000

    Fix branch switcher on android

--- lib/widgets/chat_widgets/branch_switcher.dart
+++ lib/widgets/chat_widgets/branch_switcher.dart
@@ -14,41 +14,38 @@ class BranchSwitcherState extends State<BranchSwitcher> {
     int currentIndex = MessageManager.index(widget.key!);
     int siblingCount = MessageManager.siblingCount(widget.key!);
 
-    return SizedBox(
-      width: 120,
-      height: 40,
-      child: Container(
-        alignment: Alignment.center,
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        padding: const EdgeInsets.all(0),
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.tertiary,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                padding: const EdgeInsets.all(0),
-                onPressed: () {
-                  MessageManager.last(widget.key!);
-                  setState(() {});
-                },
-                icon: const Icon(Icons.arrow_left),
-              ),
-              Text('$currentIndex/${siblingCount-1}'),
-              IconButton(
-                padding: const EdgeInsets.all(0),
-                onPressed: () {
-                  MessageManager.next(widget.key!);
-                  setState(() {});
-                },
-                icon: const Icon(Icons.arrow_right),
-              ),
-            ],
+    return Container(
+      alignment: Alignment.center,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(0),
+      width: 150,
+      height: 30,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.tertiary,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          IconButton(
+            padding: const EdgeInsets.all(0),
+            onPressed: () {
+              MessageManager.last(widget.key!);
+              setState(() {});
+            },
+            icon: const Icon(Icons.arrow_left),
           ),
-        ),
+          Text('$currentIndex/${siblingCount-1}'),
+          IconButton(
+            padding: const EdgeInsets.all(0),
+            onPressed: () {
+              MessageManager.next(widget.key!);
+              setState(() {});
+            },
+            icon: const Icon(Icons.arrow_right),
+          ),
+        ],
       ),
     );
   }
