edc1b80946fc8f80f1709c8a043824138dbaf075
dane madsen <dane_madsen@hotmail.com>
2023-11-25 18:29:37 +1000

    fix a few small errors

--- lib/widgets/page_bodies/chat_body.dart
+++ lib/widgets/page_bodies/chat_body.dart
@@ -110,20 +110,24 @@ class _ChatBodyState extends State<ChatBody> {
   }
 
   void updateCallback() {
-    chatWidgets.clear();
-    Map<Key, bool> history = MessageManager.history();
-    for (var key in history.keys) {
-      chatWidgets.add(ChatMessage(
-        key: key,
-        userGenerated: history[key] ?? false,
-      ));
-    }
-    _consoleScrollController.animateTo(
-      _consoleScrollController.position.maxScrollExtent,
-      duration: const Duration(milliseconds: 50),
-      curve: Curves.easeOut,
-    );
-    setState(() {});
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        chatWidgets.clear();
+        Map<Key, bool> history = MessageManager.history();
+        for (var key in history.keys) {
+          chatWidgets.add(ChatMessage(
+            key: key,
+            userGenerated: history[key] ?? false,
+          ));
+        }
+        _consoleScrollController.animateTo(
+          _consoleScrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 50),
+          curve: Curves.easeOut,
+        );
+        setState(() {});
+      }
+    });
   }
 
   @override
