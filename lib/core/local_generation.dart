5be55368b141c03f0c3afeb6e56ce1050600d55c
danemadsen <dane_madsen@hotmail.com>
2023-12-30 13:47:42 +1000

    work on #210

--- lib/core/local_generation.dart
+++ lib/core/local_generation.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
+import 'package:maid/models/isolate_message.dart';
+import 'package:maid/models/library_link.dart';
 import 'package:maid/static/generation_manager.dart';
 import 'package:maid/static/logger.dart';
 import 'package:maid/core/bindings.dart';
@@ -11,6 +13,7 @@ import 'package:maid/models/generation_options.dart';
 
 class LocalGeneration {
   static SendPort? _sendPort;
+  static ReceivePort? _receivePort;
 
   static void _maidOutputBridge(int code, Pointer<Char> buffer) {
     try {
@@ -24,7 +27,35 @@ class LocalGeneration {
     }
   }
 
-  static _libraryIsolate(SendPort port) async {
-    _sendPort = port;
+  static void _libraryIsolate(ReceivePort port) async {
+    _receivePort = port;
+
+    LibraryLink link;
+  }
+
+  static void prompt(String input, GenerationOptions options,
+      void Function(String) callback) async {
+    if (_sendPort != null) {
+      _sendPort!.send(IsolateMessage(
+        IsolateCode.prompt,
+        input: input,
+        callback: callback,
+      ));
+    }
+
+    final receivePort = ReceivePort();
+    _sendPort = receivePort.sendPort;
+    Isolate.spawn(_libraryIsolate, receivePort);
+
+    _sendPort!.send(IsolateMessage(
+      IsolateCode.start,
+      options: options,
+    ));
+
+    _sendPort!.send(IsolateMessage(
+      IsolateCode.prompt,
+      input: input,
+      callback: callback,
+    ));
   }
 }
