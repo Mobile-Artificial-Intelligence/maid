919b19ad955311e9a601fd9ca547b47eea458456
Dane Madsen <dane_madsen@hotmail.com>
2024-02-08 13:45:37 +1000

    start work on android sharing

--- lib/widgets/chat_widgets/chat_field.dart
+++ lib/widgets/chat_widgets/chat_field.dart
@@ -6,6 +6,7 @@ import 'package:maid/providers/model.dart';
 import 'package:maid/providers/session.dart';
 import 'package:maid/static/generation_manager.dart';
 import 'package:provider/provider.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 class ChatField extends StatefulWidget {
   const ChatField({super.key});
@@ -41,76 +42,78 @@ class _ChatFieldState extends State<ChatField> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<Session>(
-      builder: (context, session, child) {
-        bool isMobile = Platform.isAndroid || Platform.isIOS;
+    return Consumer<Session>(builder: (context, session, child) {
+      bool isMobile = Platform.isAndroid || Platform.isIOS;
 
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              if (session.isBusy &&
-                  context.read<Model>().apiType != ApiType.ollama)
-                const IconButton(
-                    onPressed: GenerationManager.stop,
-                    iconSize: 50,
-                    icon: Icon(
-                      Icons.stop_circle_sharp,
-                      color: Colors.red,
-                    )),
-              Expanded(
-                child: RawKeyboardListener(
-                  focusNode: FocusNode(),
-                  onKey: (event) {
-                    if (event.isKeyPressed(LogicalKeyboardKey.enter) && !isMobile) {
-                      if (event.isControlPressed) {
-                        // Insert line break when Ctrl+Enter is pressed
-                        int currentPos = _promptController.selection.baseOffset;
-                        String text = _promptController.text;
-                        String newText =
-                            "${text.substring(0, currentPos)}\n${text.substring(currentPos)}";
-                        _promptController.text = newText;
-                        // Position the cursor after the new line character
-                        _promptController.selection = TextSelection.fromPosition(
-                            TextPosition(offset: currentPos + 1));
-                      } else if (!session.isBusy) {
-                        // Submit the form when Enter is pressed without Ctrl
-                        send();
-                      }
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            if (session.isBusy &&
+                context.read<Model>().apiType != ApiType.ollama)
+              const IconButton(
+                  onPressed: GenerationManager.stop,
+                  iconSize: 50,
+                  icon: Icon(
+                    Icons.stop_circle_sharp,
+                    color: Colors.red,
+                  )),
+            Expanded(
+              child: RawKeyboardListener(
+                focusNode: FocusNode(),
+                onKey: (event) {
+                  if (event.isKeyPressed(LogicalKeyboardKey.enter) &&
+                      !isMobile) {
+                    if (event.isControlPressed) {
+                      // Insert line break when Ctrl+Enter is pressed
+                      int currentPos = _promptController.selection.baseOffset;
+                      String text = _promptController.text;
+                      String newText =
+                          "${text.substring(0, currentPos)}\n${text.substring(currentPos)}";
+                      _promptController.text = newText;
+                      // Position the cursor after the new line character
+                      _promptController.selection = TextSelection.fromPosition(
+                          TextPosition(offset: currentPos + 1));
+                    } else if (!session.isBusy) {
+                      // Submit the form when Enter is pressed without Ctrl
+                      send();
                     }
-                  },
-                  child: TextField(
-                    keyboardType: !isMobile ? TextInputType.text : TextInputType.multiline,
-                    textInputAction: !isMobile ? TextInputAction.none : TextInputAction.newline,
-                    minLines: 1,
-                    maxLines: 9,
-                    enableInteractiveSelection: true,
-                    controller: _promptController,
-                    cursorColor: Theme.of(context).colorScheme.secondary,
-                    decoration: InputDecoration(
-                      labelText: 'Prompt',
-                      hintStyle: Theme.of(context).textTheme.labelSmall,
-                    ),
+                  }
+                },
+                child: TextField(
+                  keyboardType:
+                      !isMobile ? TextInputType.text : TextInputType.multiline,
+                  textInputAction: !isMobile
+                      ? TextInputAction.none
+                      : TextInputAction.newline,
+                  minLines: 1,
+                  maxLines: 9,
+                  enableInteractiveSelection: true,
+                  controller: _promptController,
+                  cursorColor: Theme.of(context).colorScheme.secondary,
+                  decoration: InputDecoration(
+                    labelText: 'Prompt',
+                    hintStyle: Theme.of(context).textTheme.labelSmall,
                   ),
                 ),
               ),
-              IconButton(
-                  onPressed: () {
-                    if (!session.isBusy) {
-                      send();
-                    }
-                  },
-                  iconSize: 50,
-                  icon: Icon(
-                    Icons.arrow_circle_right,
-                    color: session.isBusy
-                        ? Theme.of(context).colorScheme.onPrimary
-                        : Theme.of(context).colorScheme.secondary,
-                  )),
-            ],
-          ),
-        );
-      }
-    );
+            ),
+            IconButton(
+                onPressed: () {
+                  if (!session.isBusy) {
+                    send();
+                  }
+                },
+                iconSize: 50,
+                icon: Icon(
+                  Icons.arrow_circle_right,
+                  color: session.isBusy
+                      ? Theme.of(context).colorScheme.onPrimary
+                      : Theme.of(context).colorScheme.secondary,
+                )),
+          ],
+        ),
+      );
+    });
   }
 }
