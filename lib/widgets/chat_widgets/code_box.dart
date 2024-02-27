749351eaab9905d96c7313798327460c218406cf
danemadsen <dane_madsen@hotmail.com>
2024-02-27 18:19:43 +1000

    work on code box

--- lib/widgets/chat_widgets/code_box.dart
+++ lib/widgets/chat_widgets/code_box.dart
@@ -8,45 +8,46 @@ class CodeBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,  // Allow horizontal scrolling
-              child: Row(
-                children: [
-                  SelectableText(
-                    code,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'monospace'
-                    ),
-                  ),
-                ],
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: IconButton(
+                icon: Icon(Icons.copy, color: Theme.of(context).colorScheme.secondary),
+                onPressed: () async {
+                  final ctx = context;
+                  Clipboard.setData(ClipboardData(text: code)).then((_) {
+                    ScaffoldMessenger.of(ctx).showSnackBar(
+                      const SnackBar(content: Text("Code copied to clipboard!")),
+                    );
+                  });
+                },
+                tooltip: 'Copy Code',
               ),
             ),
-          ),
+            SingleChildScrollView(
+              padding: const EdgeInsets.all(10.0),
+              scrollDirection: Axis.horizontal,  // Allow horizontal scrolling
+              child: SelectableText(
+                code,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'monospace'
+                ),
+              ),
+            ),
+          ],
         ),
-        IconButton(
-          icon: Icon(Icons.copy, color: Theme.of(context).colorScheme.secondary),
-          onPressed: () async {
-            final ctx = context;
-            Clipboard.setData(ClipboardData(text: code)).then((_) {
-              ScaffoldMessenger.of(ctx).showSnackBar(
-                const SnackBar(content: Text("Code copied to clipboard!")),
-              );
-            });
-          },
-          tooltip: 'Copy Code',
-        ),
-      ],
+      ),
     );
   }
 }
