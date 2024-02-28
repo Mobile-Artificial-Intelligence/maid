f1c70a03fc3edc5912686afaeb298beca7ac287c
danemadsen <dane_madsen@hotmail.com>
2024-02-28 19:05:31 +1000

    rm networking

--- lib/static/generation_manager.dart
+++ lib/static/generation_manager.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:langchain/langchain.dart';
@@ -13,9 +15,9 @@ import 'package:maid/providers/character.dart';
 import 'package:maid/providers/session.dart';
 import 'package:maid/static/logger.dart';
 import 'package:maid/providers/ai_platform.dart';
-import 'package:maid/static/networking.dart';
 import 'package:lan_scanner/lan_scanner.dart';
 import 'package:network_info_plus/network_info_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 class GenerationManager {
@@ -108,7 +110,7 @@ class GenerationManager {
     final character = context.read<Character>();
     final session = context.read<Session>();
 
-    bool permissionGranted = await Networking.getNearbyDevicesPermission();
+    bool permissionGranted = await getNearbyDevicesPermission();
     if (!permissionGranted) {
       return;
     }
@@ -271,7 +273,7 @@ class GenerationManager {
   static Future<List<String>> getOptions(AiPlatform ai) async {
     switch (ai.apiType) {
       case AiPlatformType.ollama:
-        bool permissionGranted = await Networking.getNearbyDevicesPermission();
+        bool permissionGranted = await getNearbyDevicesPermission();
         if (!permissionGranted) {
           return [];
         }
@@ -308,7 +310,7 @@ class GenerationManager {
   }
 
   static Future<String> getOllamaUrl() async {
-    bool permissionGranted = await Networking.getNearbyDevicesPermission();
+    bool permissionGranted = await getNearbyDevicesPermission();
     if (!permissionGranted) {
       return '';
     }
@@ -353,5 +355,35 @@ class GenerationManager {
     }
 
     return '';
+  }
+
+  static Future<bool> getNearbyDevicesPermission() async {
+    if (!Platform.isAndroid && !Platform.isIOS) {
+      return true;
+    }
+    
+    // Get sdk version
+    final sdk = await DeviceInfoPlugin().androidInfo.then((value) => value.version.sdkInt);
+    var permissions = <Permission>[]; // List of permissions to request
+
+    if (sdk <= 32) {
+      // ACCESS_FINE_LOCATION is required
+      permissions.add(Permission.location);
+    } else {
+      // NEARBY_WIFI_DEVICES is required
+      permissions.add(Permission.nearbyWifiDevices);
+    }
+
+    // Request permissions and check if all are granted
+    Map<Permission, PermissionStatus> statuses = await permissions.request();
+    bool allPermissionsGranted = statuses.values.every((status) => status.isGranted);
+
+    if (allPermissionsGranted) {
+      Logger.log("Nearby Devices - permission granted");
+      return true;
+    } else {
+      Logger.log("Nearby Devices - permission denied");
+      return false;
+    }
   }
 }
