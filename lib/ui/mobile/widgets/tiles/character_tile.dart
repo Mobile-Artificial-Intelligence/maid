49514082c78fbfadd05795376e3ea36d51355c6e
Dane Madsen <dane_madsen@hotmail.com>
2024-03-07 21:23:24 +1000

    charactertile

--- lib/ui/mobile/widgets/tiles/character_tile.dart
+++ lib/ui/mobile/widgets/tiles/character_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maid/providers/character.dart';
+import 'package:maid/ui/mobile/pages/character_page.dart';
 import 'package:provider/provider.dart';
 
 class CharacterTile extends StatelessWidget {
@@ -11,14 +12,25 @@ class CharacterTile extends StatelessWidget {
       builder: (context, character, child) {
         return ListTile(
             title: Column(children: [
-          CircleAvatar(
-            backgroundImage: const AssetImage("assets/maid.png"),
-            foregroundImage: Image.file(character.profile).image,
-            radius: 30,
-          ),
-          const SizedBox(height: 10.0),
-          Text(character.name, textAlign: TextAlign.center),
-        ]));
+              CircleAvatar(
+                backgroundImage: const AssetImage("assets/maid.png"),
+                foregroundImage: Image.file(character.profile).image,
+                radius: 30,
+              ),
+              const SizedBox(height: 10.0),
+              Text(character.name, textAlign: TextAlign.center),
+            ]),
+            onTap: () {
+              Navigator.pop(context); // Close the drawer
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CharacterPage()));
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            tileColor: Theme.of(context).colorScheme.primary);
       },
     );
   }
