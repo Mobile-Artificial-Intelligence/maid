59f7450ece83f339752d741ac2e5a8dfa37f6145
danemadsen <dane_madsen@hotmail.com>
2024-03-25 21:50:07 +1000

    issue with character tile description

--- lib/ui/mobile/widgets/tiles/character_tile.dart
+++ lib/ui/mobile/widgets/tiles/character_tile.dart
@@ -11,6 +11,16 @@ class CharacterTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer2<User, Character>(
       builder: (context, user, character, child) {
+        String title = Utilities.formatPlaceholders(
+          character.description, 
+          user.name, 
+          character.name
+        ).substring(0, 100);
+
+        if (title.length >= 200) {
+          title += '...';
+        }
+
         return Column(children: [
           Text("Character - ${character.name}"),
           const SizedBox(height: 10.0),
@@ -22,11 +32,7 @@ class CharacterTile extends StatelessWidget {
             ),
             minLeadingWidth: 60,
             title: Text(
-              Utilities.formatPlaceholders(
-                character.description, 
-                user.name, 
-                character.name
-              ), 
+              title, 
               style: const TextStyle(fontSize: 12.0)
             )
           )
--- lib/ui/mobile/widgets/tiles/session_tile.dart
+++ lib/ui/mobile/widgets/tiles/session_tile.dart
@@ -20,7 +20,7 @@ class _SessionTileState extends State<SessionTile> {
       builder: (context, session, child) {
         String displayMessage = widget.session.rootMessage;
         if (displayMessage.length > 30) {
-          displayMessage = '${displayMessage.substring(0, 15)}...';
+          displayMessage = '${displayMessage.substring(0, 30)}...';
         }
 
         return GestureDetector(
