f55653776612009e49fc65e73fd344cc66d58cb3
danemadsen <dane_madsen@hotmail.com>
2024-03-30 13:01:08 +1000

    more models

--- lib/classes/mistral_ai_model.dart
+++ lib/classes/mistral_ai_model.dart
@@ -1,32 +1,25 @@
 import 'package:langchain/langchain.dart';
-import 'package:langchain_openai/langchain_openai.dart';
+import 'package:langchain_mistralai/langchain_mistralai.dart';
 import 'package:maid/classes/large_language_model.dart';
 import 'package:maid/static/logger.dart';
 
-class OpenAiModel extends LargeLanguageModel {
+class MistralAiModel extends LargeLanguageModel {
   late String name;
   late String url;
   late String token;
-
-  late int nPredict;
   late double topP;
-  late double penaltyPresent;
-  late double penaltyFreq;
 
-  OpenAiModel({
+  MistralAiModel({
     super.useDefault,
     super.seed,
     super.temperature,
     this.name = '',
     this.url = '',
     this.token = '',
-    this.nPredict = 512,
-    this.topP = 0.95,
-    this.penaltyPresent = 0.0,
-    this.penaltyFreq = 0.0
+    this.topP = 0.95
   });
 
-  OpenAiModel.fromJson(Map<String, dynamic> json) {
+  MistralAiModel.fromJson(Map<String, dynamic> json) {
     fromJson(json);
   }
 
@@ -36,10 +29,7 @@ class OpenAiModel extends LargeLanguageModel {
     name = json['name'] ?? '';
     url = json['url'] ?? '';
     token = json['token'] ?? '';
-    nPredict = json['nPredict'] ?? 512;
     topP = json['topP'] ?? 0.95;
-    penaltyPresent = json['penaltyPresent'] ?? 0.0;
-    penaltyFreq = json['penaltyFreq'] ?? 0.0;
   }
 
   @override
@@ -49,27 +39,21 @@ class OpenAiModel extends LargeLanguageModel {
       'name': name,
       'url': url,
       'token': token,
-      'nPredict': nPredict,
-      'topP': topP,
-      'penaltyPresent': penaltyPresent,
-      'penaltyFreq': penaltyFreq
+      'topP': topP
     };
   }
 
   @override
   Stream<String> prompt(List<ChatMessage> messages) async* {
     try {
-      final chat = ChatOpenAI(
-        baseUrl: url,
+      final chat = ChatMistralAI(
+        baseUrl: '$url/v1',
         apiKey: token,
-        defaultOptions: ChatOpenAIOptions(
+        defaultOptions: ChatMistralAIOptions(
           model: name,
+          topP: topP,
           temperature: temperature,
-          frequencyPenalty: penaltyFreq,
-          presencePenalty: penaltyPresent,
-          maxTokens: nPredict,
-          topP: topP
-        )
+        ),
       );
 
       final stream = chat.stream(PromptValue.chat(messages));
