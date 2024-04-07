e21f190ef45f5a536f883cb4d5a62f1348a8b903
danemadsen <dane_madsen@hotmail.com>
2024-04-07 18:59:59 +1000

    themeing work

--- lib/static/themes.dart
+++ lib/static/themes.dart
@@ -10,6 +10,7 @@ class Themes {
       secondary: Colors.blue,
       tertiary: Colors.blue.shade900,
       inversePrimary: const Color.fromARGB(255, 100, 20, 20),
+      isDark: false,
     );
   }
 
@@ -22,6 +23,7 @@ class Themes {
       secondary: Colors.blue,
       tertiary: Colors.blue.shade900,
       inversePrimary: const Color.fromARGB(255, 100, 20, 20),
+      isDark: true,
     );
   }
 
@@ -33,42 +35,28 @@ class Themes {
     required Color secondary,
     required Color tertiary,
     required Color inversePrimary,
+    required bool isDark,
   }) {
     return ThemeData(
-      filledButtonTheme: FilledButtonThemeData(
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(secondary),
-        ),
-      ),
-
-      textSelectionTheme: TextSelectionThemeData(
-        selectionHandleColor: secondary,
-        selectionColor: tertiary,
-        cursorColor: secondary,
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        contentPadding: const EdgeInsets.symmetric(
-            horizontal: 20.0, vertical: 15.0), // Padding inside the TextField
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30.0),
-          borderSide: BorderSide.none,
-        ),
-        labelStyle: const TextStyle(
-            fontWeight: FontWeight.normal, color: Colors.grey, fontSize: 15.0),
-        hintStyle: TextStyle(
-            fontWeight: FontWeight.normal, color: onPrimary, fontSize: 15.0),
-        fillColor: primary,
-        filled: true,
-        floatingLabelBehavior: FloatingLabelBehavior.never,
-      ),
       dialogTheme: DialogTheme(
-        backgroundColor: background,
         shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          borderRadius: BorderRadius.all(Radius.circular(20.0))
+        ),
         titleTextStyle: TextStyle(
           color: onPrimary,
           fontSize: 20.0,
         ),
+      ),
+      filledButtonTheme: FilledButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+          backgroundColor: MaterialStateProperty.all(secondary),
+        ),
+      ),
+      switchTheme: SwitchThemeData(
+        thumbColor: MaterialStateProperty.all(Colors.white),
+        trackColor: MaterialStateProperty.all(secondary),
+        trackOutlineColor: MaterialStateProperty.all(secondary)
       ),
       sliderTheme: SliderThemeData(
         activeTrackColor: tertiary,
@@ -76,19 +64,41 @@ class Themes {
         thumbColor: secondary,
         overlayColor: tertiary,
       ),
-      navigationRailTheme: NavigationRailThemeData(
-          backgroundColor: onBackground,
-          indicatorColor: background,
-          selectedIconTheme: IconThemeData(color: secondary)),
-      colorScheme: ColorScheme.dark(
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 20.0, 
+          vertical: 15.0
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
+        fillColor: primary,
+        filled: true,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+      ),
+      textSelectionTheme: TextSelectionThemeData(
+        selectionHandleColor: secondary,
+        selectionColor: tertiary,
+        cursorColor: secondary,
+      ),
+      colorScheme: isDark ? ColorScheme.dark(
         background: background,
         onBackground: onBackground,
         primary: primary,
         onPrimary: onPrimary,
         secondary: secondary,
         tertiary: tertiary,
-        inversePrimary: const Color.fromARGB(255, 100, 20, 20),
-      ),
+        inversePrimary: inversePrimary,
+      ) : ColorScheme.light(
+        background: background,
+        onBackground: onBackground,
+        primary: primary,
+        onPrimary: onPrimary,
+        secondary: secondary,
+        tertiary: tertiary,
+        inversePrimary: inversePrimary,
+      )
     );
   }
 }
