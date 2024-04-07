aec828bb3ab0902e32aa26281c12ba1d83b30ae2
danemadsen <dane_madsen@hotmail.com>
2024-04-07 19:30:44 +1000

    LlmDropdown

--- lib/ui/mobile/widgets/appbars/home_app_bar.dart
+++ lib/ui/mobile/widgets/appbars/home_app_bar.dart
@@ -8,7 +8,7 @@ import 'package:maid/ui/mobile/pages/platforms/llama_cpp_page.dart';
 import 'package:maid/ui/mobile/pages/platforms/mistralai_page.dart';
 import 'package:maid/ui/mobile/pages/platforms/ollama_page.dart';
 import 'package:maid/ui/mobile/pages/platforms/openai_page.dart';
-import 'package:maid/ui/mobile/widgets/dropdowns/ai_platform_dropdown.dart';
+import 'package:maid/ui/mobile/widgets/dropdowns/llm_dropdown.dart';
 
 class HomeAppBar extends StatefulWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
@@ -31,7 +31,7 @@ class _HomeAppBarState extends State<HomeAppBar> {
       elevation: 0.0,
       actions: [
         const SizedBox(width: 34),
-        const AiPlatformDropdown(),
+        const LlmDropdown(),
         const Expanded(child: SizedBox()),
         IconButton(
           key: iconButtonKey, // Assign the GlobalKey to your IconButton
