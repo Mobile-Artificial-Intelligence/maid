a06a9f4bf3da6ab6be166b55218b85217c1a4688
Dane Madsen <dane_madsen@hotmail.com>
2024-04-09 16:51:36 +1000

    start work on anthropic claude model

---
+++ lib/classes/anthropic_claude_model.dart
@@ -0,0 +1,143 @@
+import 'dart:convert';
+import 'dart:ui';
+
+import 'package:http/http.dart';
+import 'package:maid/classes/large_language_model.dart';
+import 'package:maid/static/logger.dart';
+import 'package:maid_llm/maid_llm.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ClaudeModel extends LargeLanguageModel {
+  @override
+  LargeLanguageModelType get type => LargeLanguageModelType.mistralAI;
+
+  ClaudeModel({
+    super.listener, 
+    super.name,
+    super.uri = 'https://api.anthropic.com',
+    super.token,
+    super.useDefault,
+    super.seed,
+    super.temperature,
+    super.topK,
+    super.topP
+  });
+
+  ClaudeModel.fromMap(VoidCallback listener, Map<String, dynamic> json) {
+    addListener(listener);
+    fromMap(json);
+  }
+
+  @override
+  void fromMap(Map<String, dynamic> json) {
+    if (json['uri'] == null) json['uri'] = 'https://api.anthropic.com';
+    super.fromMap(json);
+    notifyListeners();
+  }
+
+  @override
+  Stream<String> prompt(List<ChatNode> messages) async* {
+    List<Map<String, dynamic>> chat = [];
+
+    for (var message in messages) {
+      chat.add({
+        'role': message.role.name,
+        'content': message.content,
+      });
+    }
+
+    try {
+      final url = Uri.parse('$uri/v1/messages');
+      
+      final headers = {
+        'x-api-key': token,
+        'anthropic-version': '2023-06-01',
+        'content-type': 'application/json',
+      };
+
+      final body = {
+        'model': name,
+        'messages': chat,
+        'temperature': temperature,
+        'top_k': topK,
+        'top_p': topP,
+        'max_tokens': nPredict,
+        'stream': true,
+        'random_seed': seed
+      };
+      
+      final request = Request("POST", url)
+        ..headers.addAll(headers)
+        ..body = json.encode(body);
+
+      final response = await request.send();
+
+      final stream = response.stream
+          .transform(utf8.decoder)
+          .transform(const LineSplitter());
+
+      await for (final line in stream) {
+        final data = json.decode(line);
+        Logger.log('Data: $data');
+
+        final content = data['choices'][0]['delta']['content'] as String?;
+
+        if (content != null && content.isNotEmpty) {
+          yield content;
+        }
+      }
+    } catch (e) {
+      Logger.log('Error: $e');
+    }
+  }
+  
+  @override
+  Future<void> updateOptions() async {
+    try {
+      final url = Uri.parse('$uri/v1/models');
+      
+      final headers = {
+        "Accept": "application/json",
+        'Authorization':'Bearer $token',
+      };
+
+      final request = Request("GET", url)
+        ..headers.addAll(headers);
+
+      final response = await request.send();
+
+      if (response.statusCode == 200) {
+        final body = await response.stream.bytesToString();
+        final data = json.decode(body);
+        Logger.log('Data: $data');
+
+        final models = data['data'] as List<dynamic>?;
+
+        if (models != null) {
+          options = models.map((model) => model['id'] as String).toList();
+        } else {
+          options = [];
+        }
+      } else {
+        throw Exception('Failed to update options: ${response.statusCode}');
+      }
+    } catch (e) {
+      Logger.log('Error: $e');
+    }
+  }
+  
+  @override
+  Future<void> resetUri() async {
+    uri = 'https://api.anthropic.com';
+
+    await updateOptions();
+    notifyListeners();
+  }
+
+  @override
+  void save() {
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setString("mistral_ai_model", json.encode(toMap()));
+    });
+  }
+}
