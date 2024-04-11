968cf4b7a2fc3a245e151eaa5b9e91e752960a79
danemadsen <dane_madsen@hotmail.com>
2024-04-11 23:55:00 +1000

    add setup scripts

---
+++ setup_flutter.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: Get the current directory of the script
+set "script_dir=%~dp0"
+
+:: Remove trailing backslash
+set "flutter_path=%script_dir:~0,-1%\flutter\bin"
+
+:: Add Flutter to the PATH
+set PATH=%flutter_path%;%PATH%
+
+echo Flutter path set to %flutter_path%
---
+++ setup_flutter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the current directory of the script
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Add Flutter to the PATH
+export PATH="$DIR/flutter/bin:$PATH"
+
+echo "Flutter path set to $DIR/flutter/bin"
