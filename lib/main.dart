ad43b0addd852c93bc41ed2d4850e42c4e639f35
danemadsen <dane_madsen@hotmail.com>
2024-05-26 23:38:47 +1000

    save thememode

--- lib/main.dart
+++ lib/main.dart
@@ -22,27 +22,29 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  AppPreferences appPreferences = await AppPreferences.last;
   User user = await User.last;
   Character character = await Character.last;
   Session session = await Session.last;
 
   runApp(
-    MaidApp(user: user, character: character, session: session)
+    MaidApp(user: user, character: character, session: session, appPreferences: appPreferences)
   );
 }
 
 class MaidApp extends StatelessWidget {
+  final AppPreferences appPreferences;
   final User user;
   final Character character;
   final Session session;
 
-  const MaidApp({super.key, required this.user, required this.character, required this.session});
+  const MaidApp({super.key, required this.user, required this.character, required this.session, required this.appPreferences});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => AppPreferences()),
+        ChangeNotifierProvider(create: (context) => appPreferences),
         ChangeNotifierProvider(create: (context) => user),
         ChangeNotifierProvider(create: (context) => character),
         ChangeNotifierProvider(create: (context) => session),
--- lib/providers/app_preferences.dart
+++ lib/providers/app_preferences.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppPreferences extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.dark;
 
   ThemeMode get themeMode => _themeMode;
 
+  AppPreferences(ThemeMode themeMode) : _themeMode = themeMode;
+
+  static Future<AppPreferences> get last async {
+    final prefs = await SharedPreferences.getInstance();
+
+    int themeModeIndex = prefs.getInt("theme_mode") ?? ThemeMode.dark.index;
+
+    return AppPreferences(ThemeMode.values[themeModeIndex]);
+  }
+
+  Future<void> save() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setInt("theme_mode", _themeMode.index);
+  }
+
   set themeMode(ThemeMode value) {
     _themeMode = value;
+    save();
     notifyListeners();
   }
 
