0ad84ee42f41100da766d2438af1359895cea9ba
danemadsen <dane_madsen@hotmail.com>
2024-06-11 22:19:28 +1000

    side panel stuff

--- lib/ui/desktop/widgets/side_bar.dart
+++ lib/ui/desktop/widgets/side_bar.dart
@@ -12,16 +12,39 @@ class SideBar extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 10),
       width: 50,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IconButton(
-            color: Theme.of(context).colorScheme.inversePrimary,
-            icon: const Icon(Icons.view_sidebar_rounded),
-            onPressed: () {
-              context.read<DesktopLayout>().toggleSidePanel();
-            },
-          ),
+          buildTopButtons(context),
+          buildBottomButtons(context),
         ],
       ),
+    );
+  }
+
+  Widget buildTopButtons(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          color: Theme.of(context).colorScheme.inversePrimary,
+          icon: const Icon(Icons.view_sidebar_rounded),
+          onPressed: () {
+            context.read<DesktopLayout>().toggleSidePanel();
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget buildBottomButtons(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.terminal_rounded),
+          onPressed: () {
+            context.read<DesktopLayout>().toggleTerminal();
+          },
+        ),
+      ],
     );
   }
 }
