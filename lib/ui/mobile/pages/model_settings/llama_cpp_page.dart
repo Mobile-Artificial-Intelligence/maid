f4c111452ec38d990ea66141657418ff7e99190e
danemadsen <dane_madsen@hotmail.com>
2024-06-18 16:23:23 +1000

    llama_cpp_page refactor

--- lib/ui/mobile/pages/model_settings/llama_cpp_page.dart
+++ lib/ui/mobile/pages/model_settings/llama_cpp_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maid/classes/large_language_model.dart';
 import 'package:maid/classes/llama_cpp_model.dart';
 import 'package:maid/providers/app_data.dart';
 import 'package:maid/providers/session.dart';
@@ -49,90 +50,104 @@ class _LlamaCppPageState extends State<LlamaCppPage> {
     return Scaffold(
       appBar: const GenericAppBar(title: "LlamaCPP Parameters"),
       body: SessionBusyOverlay(
-        child: Consumer<AppData>(
-          builder: (context, appData, child) {
-            session = appData.currentSession;
+        child: buildListView(context),
+      )
+    );
+  }
 
-            return ListView(
-              children: [
-                Text(
-                  "Model: ${session.model.name}",
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 15.0),
-                Wrap(
-                  alignment: WrapAlignment.center,
-                  spacing: 8,
-                  runSpacing: 4,
-                  children: [
-                    FilledButton(
-                      onPressed: () {
-                        session.model.reset();
-                      },
-                      child: const Text(
-                        "Reset"
-                      ),
-                    ),
-                    FilledButton(
-                      onPressed: () {
-                        storageOperationDialog(
-                          context, 
-                          (session.model as LlamaCppModel).loadModel
-                        );
-                        session.notify();
-                      },
-                      child: const Text(
-                        "Load GGUF"
-                      ),
-                    ),
-                    FilledButton(
-                      onPressed: () {
-                        session.model.resetUri();
-                        session.notify();
-                      },
-                      child: const Text(
-                        "Unload GGUF"
-                      ),
-                    ),
-                  ],
-                ),
-                Divider(
-                  height: 20,
-                  indent: 10,
-                  endIndent: 10,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                const TemplateParameter(),
-                Divider(
-                  height: 20,
-                  indent: 10,
-                  endIndent: 10,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                const PenalizeNlParameter(),
-                const SeedParameter(),
-                const TemperatureParameter(),
-                const TopKParameter(),
-                const TopPParameter(),
-                const MinPParameter(),
-                const TfsZParameter(),
-                const TypicalPParameter(),
-                const LastNPenaltyParameter(),
-                const RepeatPenaltyParameter(),
-                const FrequencyPenaltyParameter(),
-                const PresentPenaltyParameter(),
-                const MirostatParameter(),
-                const MirostatTauParameter(),
-                const MirostatEtaParameter(),
-                const NCtxParameter(),
-                const NPredictParameter(),
-                const NBatchParameter(),
-                const NThreadsParameter(),
-              ]
+  Widget buildListView(BuildContext context) {
+    return ListView(
+      children: [
+        buildModelName(),
+        const SizedBox(height: 15.0),
+        buildButtons(context),
+        Divider(
+          height: 20,
+          indent: 10,
+          endIndent: 10,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        const TemplateParameter(),
+        Divider(
+          height: 20,
+          indent: 10,
+          endIndent: 10,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        const PenalizeNlParameter(),
+        const SeedParameter(),
+        const TemperatureParameter(),
+        const TopKParameter(),
+        const TopPParameter(),
+        const MinPParameter(),
+        const TfsZParameter(),
+        const TypicalPParameter(),
+        const LastNPenaltyParameter(),
+        const RepeatPenaltyParameter(),
+        const FrequencyPenaltyParameter(),
+        const PresentPenaltyParameter(),
+        const MirostatParameter(),
+        const MirostatTauParameter(),
+        const MirostatEtaParameter(),
+        const NCtxParameter(),
+        const NPredictParameter(),
+        const NBatchParameter(),
+        const NThreadsParameter(),
+      ]
+    );
+  }
+
+  Widget buildButtons(BuildContext context) {
+    return Wrap(
+      alignment: WrapAlignment.center,
+      spacing: 8,
+      runSpacing: 6,
+      children: [
+        FilledButton(
+          onPressed: () {
+            LargeLanguageModel.of(context).reset();
+          },
+          child: const Text(
+            "Reset"
+          ),
+        ),
+        FilledButton(
+          onPressed: () {
+            storageOperationDialog(
+              context, 
+              LlamaCppModel.of(context).loadModel
             );
           },
+          child: const Text(
+            "Load GGUF"
+          ),
         ),
-      )
+        FilledButton(
+          onPressed: () {
+            LargeLanguageModel.of(context).resetUri();
+          },
+          child: const Text(
+            "Unload GGUF"
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildModelName() {
+    return Consumer<AppData>(
+      builder: (context, appData, child) {
+        final session = appData.currentSession;
+
+        if (session.model.name.isEmpty) {
+          return const SizedBox.shrink();
+        }
+
+        return Text(
+          "Model: ${session.model.name}",
+          textAlign: TextAlign.center,
+        );
+      }
     );
   }
 }
