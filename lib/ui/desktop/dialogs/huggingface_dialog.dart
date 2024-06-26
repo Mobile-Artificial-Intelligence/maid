099d24017363e20ffaf49f5c702f17ff7454904f
danemadsen <dane_madsen@hotmail.com>
2024-06-26 17:51:24 +1000

    select model

--- lib/ui/desktop/dialogs/huggingface_dialog.dart
+++ lib/ui/desktop/dialogs/huggingface_dialog.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:maid/classes/providers/huggingface_selection.dart';
+import 'package:maid/classes/providers/large_language_models/llama_cpp_model.dart';
 import 'package:maid/ui/desktop/dropdowns/huggingface_model_dropdown.dart';
+import 'package:provider/provider.dart';
 
 class HuggingfaceDialog extends StatelessWidget {
   const HuggingfaceDialog({super.key});
@@ -14,15 +16,7 @@ class HuggingfaceDialog extends StatelessWidget {
       ),
       content: const HuggingfaceModelDropdown(),
       actions: [
-        FilledButton(
-          onPressed: () {
-            HuggingfaceSelection.of(context).download();
-            Navigator.of(context).pop();
-          },
-          child: const Text(
-            "Download"
-          ),
-        ),
+        buildButton(),
         FilledButton(
           onPressed: () => Navigator.of(context).pop(),
           child: const Text(
@@ -31,6 +25,54 @@ class HuggingfaceDialog extends StatelessWidget {
         ),
       ],
       actionsAlignment: MainAxisAlignment.center,
+    );
+  }
+
+  Widget buildButton() {
+    return Consumer<HuggingfaceSelection>(
+      builder: (context, huggingfaceSelection, child) {
+        return FutureBuilder(
+          future: huggingfaceSelection.alreadyExists, 
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (!snapshot.data!) {
+                return buildDownloadButton(context, huggingfaceSelection);
+              }
+              else {
+                return buildSelectButton(context, huggingfaceSelection);
+              }
+            }
+            else {
+              return const CircularProgressIndicator();
+            }
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildDownloadButton(BuildContext context, HuggingfaceSelection huggingfaceSelection) {
+    return FilledButton(
+      onPressed: () {
+        huggingfaceSelection.download();
+        Navigator.of(context).pop();
+      },
+      child: const Text(
+        "Download"
+      ),
+    );
+  }
+
+  Widget buildSelectButton(BuildContext context, HuggingfaceSelection huggingfaceSelection) {
+    return FilledButton(
+      onPressed: () {
+        LlamaCppModel.of(context).uri = huggingfaceSelection.filePath!;
+        LlamaCppModel.of(context).name = huggingfaceSelection.tagValue!;
+        Navigator.of(context).pop();
+      },
+      child: const Text(
+        "Select"
+      ),
     );
   }
 }
