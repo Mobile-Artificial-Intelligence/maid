47c0fc921c4958302cb1e42d57fd070feae94824
danemadsen <dane_madsen@hotmail.com>
2024-07-04 21:00:58 +1000

    semi working

--- .github/scripts/create_dmg.sh
+++ .github/scripts/create_dmg.sh
@@ -7,6 +7,12 @@ APP_DIR="build/macos/Build/Products/Release/${APP_NAME}.app"
 DMG_DIR="dmg"
 TMP_MOUNT="/tmp/Maid"
 DMG_BACKGROUND_IMG="./media/dmg.png"
+WINX=0
+WINY=0
+WINW=700
+WINH=1000
+ICON_SIZE=64
+TEXT_SIZE=12
 
 # Create the dmg and app directories if they do not exist
 mkdir -p ${DMG_DIR}
@@ -19,33 +25,82 @@ hdiutil create ${DMG_DIR}/${APP_NAME}_tmp.dmg -volname "${APP_NAME}" -srcfolder 
 hdiutil attach ${DMG_DIR}/${APP_NAME}_tmp.dmg -mountpoint ${TMP_MOUNT}
 
 # Copy background image
-mkdir ${TMP_MOUNT}/.background
+mkdir -p ${TMP_MOUNT}/.background
 cp ${DMG_BACKGROUND_IMG} ${TMP_MOUNT}/.background/feature.png
 
 # Create a symbolic link to the Applications folder
 ln -s /Applications ${TMP_MOUNT}/Applications
 
-# Set the background image
+# AppleScript to set DMG properties
 osascript <<EOF
-tell application "Finder"
-  tell disk "${APP_NAME}"
-    open
-    set current view of container window to icon view
-    set toolbar visible of container window to false
-    set statusbar visible of container window to false
-    set the bounds of container window to {0, 0, 700, 1000}
-    set viewOptions to the icon view options of container window
-    set arrangement of viewOptions to not arranged
-    set icon size of viewOptions to 64
-    set background picture of viewOptions to file ".background:feature.png"
-    set position of item "${APP_NAME}.app" of container window to {150, 50}
-    set position of item "Applications" of container window to {150, 300}
-    close
-    open
-    update without registering applications
-    delay 5
-  end tell
-end tell
+on run
+    tell application "Finder"
+        tell disk "${APP_NAME}"
+            open
+
+            set theXOrigin to ${WINX}
+            set theYOrigin to ${WINY}
+            set theWidth to ${WINW}
+            set theHeight to ${WINH}
+
+            set theBottomRightX to (theXOrigin + theWidth)
+            set theBottomRightY to (theYOrigin + theHeight)
+            set dsStore to "\"" & "/Volumes/${APP_NAME}/.DS_Store\""
+
+            tell container window
+                set current view to icon view
+                set toolbar visible to false
+                set statusbar visible to false
+                set the bounds to {theXOrigin, theYOrigin, theBottomRightX, theBottomRightY}
+                set statusbar visible to false
+            end tell
+
+            set opts to the icon view options of container window
+            tell opts
+                set icon size to ${ICON_SIZE}
+                set text size to ${TEXT_SIZE}
+                set arrangement to not arranged
+            end tell
+            set background picture of opts to file ".background:feature.png"
+
+            -- Positioning
+            set position of item "${APP_NAME}.app" of container window to {150, 50}
+            set position of item "Applications" of container window to {150, 300}
+
+            close
+            open
+            -- Force saving of the size
+            delay 1
+
+            tell container window
+                set statusbar visible to false
+                set the bounds to {theXOrigin, theYOrigin, theBottomRightX - 10, theBottomRightY - 10}
+            end tell
+        end tell
+
+        delay 1
+
+        tell disk "${APP_NAME}"
+            tell container window
+                set statusbar visible to false
+                set the bounds to {theXOrigin, theYOrigin, theBottomRightX, theBottomRightY}
+            end tell
+        end tell
+
+        --give the finder some time to write the .DS_Store file
+        delay 3
+
+        set waitTime to 0
+        set ejectMe to false
+        repeat while ejectMe is false
+            delay 1
+            set waitTime to waitTime + 1
+            
+            if (do shell script "[ -f " & dsStore & " ]; echo $?") = "0" then set ejectMe to true
+        end repeat
+        log "waited " & waitTime & " seconds for .DS_Store to be created."
+    end tell
+end run
 EOF
 
 # Unmount the temporary DMG
@@ -56,3 +111,5 @@ hdiutil convert ${DMG_DIR}/${APP_NAME}_tmp.dmg -format UDZO -o ${DMG_DIR}/${APP_
 
 # Remove temporary DMG
 rm ${DMG_DIR}/${APP_NAME}_tmp.dmg
+
+echo "DMG creation completed successfully!"
