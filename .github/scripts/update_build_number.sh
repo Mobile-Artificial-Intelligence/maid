18f8f7d3941ba99d2f8b17cc95d1eea08e93f1a4
danemadsen <dane_madsen@hotmail.com>
2024-07-06 16:56:16 +1000

    update build number

---
+++ .github/scripts/update_build_number.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get the number of commits
+commit_count=$(git rev-list --count HEAD)
+
+# Update the pubspec.yaml file with the new build number
+sed -i '' "s/version: \(.*\)+.*/version: \1+$commit_count/" ../../pubspec.yaml
