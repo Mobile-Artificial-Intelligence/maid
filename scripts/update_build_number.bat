28ffd147d62f424c4cd2da48ad50cc1541d0491c
danemadsen <dane_madsen@hotmail.com>
2024-07-09 11:59:18 +1000

    scripts

--- scripts/update_build_number.bat
+++ scripts/update_build_number.bat
@@ -3,4 +3,4 @@
 for /f %%i in ('git rev-list --count HEAD') do set commit_count=%%i
 
 :: Update the pubspec.yaml file with the new build number
-powershell -Command "(gc ../pubspec.yaml) -replace 'version: (.*)\+.*', 'version: $1+%commit_count%' | Out-File -encoding ASCII ../pubspec.yaml"
+powershell -Command "(gc ../pubspec.yaml) -replace 'version: (.*)\+.*', 'version: $1+%commit_count%' | Out-File -encoding ASCII ./pubspec.yaml"
