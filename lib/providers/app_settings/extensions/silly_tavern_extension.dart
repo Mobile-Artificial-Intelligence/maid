39ad1d12012df12090d4d33efd6fa803002abef8
danemadsen <dane_madsen@hotmail.com>
2025-02-20 11:16:47 +1000

    st decoding

--- lib/providers/app_settings/extensions/silly_tavern_extension.dart
+++ lib/providers/app_settings/extensions/silly_tavern_extension.dart
@@ -18,26 +18,30 @@ extension SillyTavernExtension on AppSettings {
       character = character['data'];
     }
 
-    if (character['name'] != null && character['name'] is String) {
-      final name = character['name'] as String;
+    final name = character['name'] as String?;
+    if (name != null && name.trim().isNotEmpty) {
       result += '### Character Name: ${name.titleize}\n\n';
       assistantName = name;
     }
 
-    if (character['description'] != null && character['description'] is String) {
-      result += '### Description\n\n${(character['description'] as String).titleize}\n\n';
+    final description = character['description'] as String?;
+    if (description != null && description.trim().isNotEmpty) {
+      result += '### Description\n\n${description.titleize}\n\n';
     }
 
-    if (character['personality'] != null && character['personality'] is String) {
-      result += '### Personality\n\n${(character['personality'] as String).titleize}\n\n';
+    final personality = character['personality'] as String?;
+    if (personality != null && personality.trim().isNotEmpty) {
+      result += '### Personality\n\n${personality.titleize}\n\n';
     }
 
-    if (character['scenario'] != null && character['scenario'] is String) {
-      result += '### Scenario\n\n${(character['scenario'] as String).titleize}\n\n';
+    final scenario = character['scenario'] as String?;
+    if (scenario != null && scenario.trim().isNotEmpty) {
+      result += '### Scenario\n\n${scenario.titleize}\n\n';
     }
 
-    if (character['mes_example'] != null && character['mes_example'] is String) {
-      result += '### Message Example\n\n${(character['mes_example'] as String).titleize}\n\n';
+    final example = character['mes_example'] as String?;
+    if (example != null && example.trim().isNotEmpty) {
+      result += '### Message Example\n\n${example.titleize}\n\n';
     }
 
     systemPrompt = result;
