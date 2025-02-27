ac878fc84f9f5490b731c8206e47b3776fe5a2a6
pomodori92 <thekhameleon92@gmail.com>
2025-02-27 11:44:31 +0100

    Italian localization.

--- lib/locale/app_localizations_it.dart
+++ lib/locale/app_localizations_it.dart
@@ -4,131 +4,131 @@ import 'app_localizations.dart';
 
 // ignore_for_file: type=lint
 
-/// The translations for English (`en`).
-class AppLocalizationsEn extends AppLocalizations {
-  AppLocalizationsEn([String locale = 'en']) : super(locale);
+/// The translations for Italian (`it`).
+class AppLocalizationsIt extends AppLocalizations {
+  AppLocalizationsIt([String locale = 'it']) : super(locale);
 
   @override
-  String get localeTitle => 'Locale';
+  String get localeTitle => 'Lingua';
 
   @override
-  String get defaultLocale => 'Default Locale';
+  String get defaultLocale => 'Lingua predefinita';
 
   @override
-  String get loading => 'Loading...';
+  String get loading => 'Caricamento...';
 
   @override
-  String get loadModel => 'Load Model';
+  String get loadModel => 'Carica modello';
 
   @override
-  String get noModelSelected => 'No Model Selected';
+  String get noModelSelected => 'Nessun modello selezionato';
 
   @override
-  String get noModelLoaded => 'No Model Loaded';
+  String get noModelLoaded => 'Nessun modello caricato';
 
   @override
-  String get delete => 'Delete';
+  String get delete => 'Elimina';
 
   @override
-  String get edit => 'Edit';
+  String get edit => 'Modifica';
 
   @override
-  String get regenerate => 'Regenerate';
+  String get regenerate => 'Rigenera';
 
   @override
-  String get chatsTitle => 'Chats';
+  String get chatsTitle => 'Chat';
 
   @override
-  String get newChat => 'New Chat';
+  String get newChat => 'Nuova chat';
 
   @override
-  String get anErrorOccurred => 'An error occurred';
+  String get anErrorOccurred => 'Si è verificato un errore';
 
   @override
-  String get errorTitle => 'Error';
+  String get errorTitle => 'Errore';
 
   @override
-  String get key => 'Key';
+  String get key => 'Chiave';
 
   @override
-  String get value => 'Value';
+  String get value => 'Valore';
 
   @override
   String get ok => 'OK';
 
   @override
-  String get done => 'Done';
+  String get done => 'Fatto';
 
   @override
-  String get close => 'Close';
+  String get close => 'Chiudi';
 
   @override
   String save(String label) {
-    return 'Save $label';
+    return 'Salva $label';
   }
 
   @override
-  String get next => 'Next';
+  String get next => 'Avanti';
 
   @override
-  String get previous => 'Previous';
+  String get previous => 'Precedente';
 
   @override
-  String get contentShared => 'Content Shared';
+  String get contentShared => 'Contenuto condiviso';
 
   @override
-  String get setUserImage => 'Set User Image';
+  String get setUserImage => 'Imposta immagine utente';
 
   @override
-  String get setAssistantImage => 'Set Assistant Image';
+  String get setAssistantImage => 'Imposta immagine assistente';
 
   @override
-  String get loadUserImage => 'Load User Image';
+  String get loadUserImage => 'Carica immagine utente';
 
   @override
-  String get loadAssistantImage => 'Load Assistant Image';
+  String get loadAssistantImage => 'Carica immagine assistente';
 
   @override
-  String get userName => 'User Name';
+  String get userName => 'Nome utente';
 
   @override
-  String get assistantName => 'Assistant Name';
+  String get assistantName => 'Nome assistente';
 
   @override
-  String get user => 'User';
+  String get user => 'Utente';
 
   @override
-  String get assistant => 'Assistant';
+  String get assistant => 'Assistente';
 
   @override
-  String get cancel => 'Cancel';
+  String get cancel => 'Annulla';
 
   @override
-  String get aiEcosystem => 'AI Ecosystem';
+  String get aiEcosystem => 'Ecosistema IA';
 
   @override
   String get llamaCpp => 'Llama CPP';
 
   @override
-  String get llamaCppModel => 'Llama CPP Model';
+  String get llamaCppModel => 'Modello Llama CPP';
 
   @override
-  String get remoteModel => 'Remote Model';
+  String get remoteModel => 'Modello remoto';
 
   @override
-  String get refreshRemoteModels => 'Refresh Remote Models';
+  String get refreshRemoteModels => 'Aggiorna modelli remoti';
 
   @override
   String get ollama => 'Ollama';
 
   @override
-  String get searchLocalNetwork => 'Search Local Network';
+  String get searchLocalNetwork => 'Cerca nella rete locale';
 
   @override
-  String get localNetworkSearchTitle => 'Local Network Search';
+  String get localNetworkSearchTitle => 'Ricerca in rete locale';
 
   @override
-  String get localNetworkSearchContent => 'This feature requires additional permissions to search your local network for Ollama instances.';
+  String get localNetworkSearchContent => 'Questa funzionalità richiede permessi aggiuntivi per cercare le istanze di Ollama nella rete locale.';
 
   @override
   String get openAI => 'OpenAI';
@@ -137,125 +137,125 @@ class AppLocalizationsEn extends AppLocalizations {
   String get mistral => 'Mistral';
 
   @override
-  String get selectAiEcosystem => 'Select AI Ecosystem';
+  String get selectAiEcosystem => 'Seleziona ecosistema IA';
 
   @override
-  String get selectOverrideType => 'Select Override Type';
+  String get selectOverrideType => 'Seleziona tipo di override';
 
   @override
-  String get selectRemoteModel => 'Select Remote Model';
+  String get selectRemoteModel => 'Seleziona modello remoto';
 
   @override
-  String get selectThemeMode => 'Select App Theme Mode';
+  String get selectThemeMode => 'Seleziona modalità tema dell\'app';
 
   @override
-  String get overrideTypeString => 'String';
+  String get overrideTypeString => 'Stringa';
 
   @override
-  String get overrideTypeInteger => 'Integer';
+  String get overrideTypeInteger => 'Intero';
 
   @override
-  String get overrideTypeDouble => 'Double';
+  String get overrideTypeDouble => 'Decimale';
 
   @override
-  String get overrideTypeBoolean => 'Bool';
+  String get overrideTypeBoolean => 'Booleano';
 
   @override
-  String get inferanceOverrides => 'Inference Overrides';
+  String get inferanceOverrides => 'Override di inferenza';
 
   @override
-  String get addOverride => 'Add Override';
+  String get addOverride => 'Aggiungi override';
 
   @override
-  String get saveOverride => 'Save Override';
+  String get saveOverride => 'Salva override';
 
   @override
-  String get deleteOverride => 'Delete Override';
+  String get deleteOverride => 'Elimina override';
 
   @override
-  String get themeMode => 'Theme Mode';
+  String get themeMode => 'Modalità tema';
 
   @override
-  String get themeModeSystem => 'System';
+  String get themeModeSystem => 'Sistema';
 
   @override
-  String get themeModeLight => 'Light';
+  String get themeModeLight => 'Chiaro';
 
   @override
-  String get themeModeDark => 'Dark';
+  String get themeModeDark => 'Scuro';
 
   @override
-  String get themeSeedColor => 'Theme Seed Color';
+  String get themeSeedColor => 'Colore di base del tema';
 
   @override
-  String get editMessage => 'Edit Message';
+  String get editMessage => 'Modifica messaggio';
 
   @override
-  String get settingsTitle => 'Settings';
+  String get settingsTitle => 'Impostazioni';
 
   @override
   String aiSettings(String aiControllerType) {
-    return '$aiControllerType Settings';
+    return '$aiControllerType Impostazioni';
   }
 
   @override
-  String get userSettings => 'User Settings';
+  String get userSettings => 'Impostazioni utente';
 
   @override
-  String get assistantSettings => 'Assistant Settings';
+  String get assistantSettings => 'Impostazioni assistente';
 
   @override
-  String get systemSettings => 'System Settings';
+  String get systemSettings => 'Impostazioni di sistema';
 
   @override
-  String get systemPrompt => 'System Prompt';
+  String get systemPrompt => 'Prompt di sistema';
 
   @override
-  String get clearChats => 'Clear Chats';
+  String get clearChats => 'Svuota chat';
 
   @override
-  String get resetSettings => 'Reset Settings';
+  String get resetSettings => 'Ripristina impostazioni';
 
   @override
-  String get clearCache => 'Clear Cache';
+  String get clearCache => 'Svuota cache';
 
   @override
-  String get aboutTitle => 'About';
+  String get aboutTitle => 'Informazioni';
 
   @override
-  String get aboutContent => 'Maid is a cross-platform free and open source application for interfacing with llama.cpp models locally, and remotely with Ollama, Mistral, and OpenAI models remotely. Maid supports sillytavern character cards to allow you to interact with all your favorite characters. Maid supports downloading a curated list of Models in-app directly from huggingface. Maid is distributed under the MIT licence and is provided without warrenty of any kind, express or implied. Maid is not affiliated with Huggingface, Meta (Facebook), MistralAi, OpenAI, Google, Microsoft or any other company providing a model compatible with this application.';
+  String get aboutContent => 'Maid è un\'applicazione multipiattaforma gratuita e open source per interfacciarsi localmente con i modelli di llama.cpp, e da remoto con i modelli di Ollama, Mistral e OpenAI. Maid supporta le schede dei personaggi di sillytavern per permetterti di interagire con tutti i tuoi personaggi preferiti. Maid consente di scaricare, direttamente dall\'app, una lista curata di modelli da huggingface. Maid è distribuito sotto la licenza MIT ed è fornito senza alcuna garanzia, esplicita o implicita. Maid non è affiliato con Huggingface, Meta (Facebook), MistralAi, OpenAI, Google, Microsoft o con qualsiasi altra azienda che fornisce un modello compatibile con questa applicazione.';
 
   @override
-  String get leadMaintainer => 'Lead Maintainer';
+  String get leadMaintainer => 'Manutentore principale';
 
   @override
-  String get apiKey => 'API Key';
+  String get apiKey => 'Chiave API';
 
   @override
-  String get baseUrl => 'Base URL';
+  String get baseUrl => 'URL base';
 
   @override
-  String get clearPrompt => 'Clear Prompt';
+  String get clearPrompt => 'Pulisci prompt';
 
   @override
-  String get submitPrompt => 'Submit Prompt';
+  String get submitPrompt => 'Invia prompt';
 
   @override
-  String get stopPrompt => 'Stop Prompt';
+  String get stopPrompt => 'Ferma prompt';
 
   @override
-  String get typeMessage => 'Type a message...';
+  String get typeMessage => 'Scrivi un messaggio...';
 
   @override
-  String get code => 'Code';
+  String get code => 'Codice';
 
   @override
   String copyLabel(String label) {
-    return 'Copy $label';
+    return 'Copia $label';
   }
 
   @override
   String labelCopied(String label) {
-    return '$label copied to clipboard!';
+    return '$label copiato negli appunti!';
   }
 }
