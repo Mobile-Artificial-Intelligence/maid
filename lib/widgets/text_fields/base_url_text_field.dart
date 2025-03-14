dda47cad705d0c536d15e36ed6c507138b9b7c74
danemadsen <dane_madsen@hotmail.com>
2025-03-14 12:46:09 +1000

    disallow localhost on web

--- lib/widgets/text_fields/base_url_text_field.dart
+++ lib/widgets/text_fields/base_url_text_field.dart
@@ -8,11 +8,29 @@ class BaseUrlTextField extends StatelessWidget {
     required this.aiController,
   });
 
+  void onChanged(BuildContext context, String value) {
+    if (kIsWeb  && value.contains('localhost')) {
+      showDialog(
+        context: context,
+        builder: exceptionBuilder,
+      );
+    }
+
+    aiController.baseUrl = value;
+  }
+
+  Widget exceptionBuilder(BuildContext context) => ErrorDialog(
+    exception: PlatformException(
+      code: 'localhost', 
+      message: 'localhost is not allowed on web'
+    ),
+  );
+
   @override
   Widget build(BuildContext context) => ListenableTextField(
     listenable: aiController,
     selector: () => aiController.baseUrl, 
-    onChanged: (value) => aiController.baseUrl = value,
+    onChanged: (value) => onChanged(context, value),
     labelText: AppLocalizations.of(context)!.baseUrl,
     requireSave: true,
   );
