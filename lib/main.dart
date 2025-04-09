89a540fd5a64ae2c1687bc9fca7e7ae8fc16878a
danemadsen <dane_madsen@hotmail.com>
2025-04-09 11:19:53 +1000

    web

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:llama_sdk/llama_sdk.dart'
-    if (dart.library.html) 'package:lcpp/lcpp.web.dart';
+    if (dart.library.html) 'package:llama_sdk/llama_sdk.web.dart';
 import 'package:tree_structs/tree_structs.dart';
 import 'package:ollama_dart/ollama_dart.dart' as ollama;
 import 'package:openai_dart/openai_dart.dart' as open_ai;
