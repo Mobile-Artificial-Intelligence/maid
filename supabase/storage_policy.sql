86500daf9e72400ef889737aa73db85ef41d3b27
danemadsen <dane_madsen@hotmail.com>
2025-04-18 11:22:33 +1000

    supabase policies

---
+++ supabase/storage_policy.sql
@@ -0,0 +1,20 @@
+-- Enable RLS for storage
+alter table storage.objects enable row level security;
+
+-- Allow user to read *only their own profile image*
+create policy "Users can read their own profile image"
+on storage.objects for select
+using (
+  bucket_id = 'avatars'
+  AND auth.uid() IS NOT NULL
+  AND storage.objects.name = auth.uid() || '.jpg'
+);
+
+-- Allow user to insert *only their own profile image*
+create policy "Users can upload their own profile image"
+on storage.objects for insert
+with check (
+  bucket_id = 'avatars'
+  AND auth.uid() IS NOT NULL
+  AND storage.objects.name = auth.uid() || '.jpg'
+);
