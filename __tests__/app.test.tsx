73807ee9856bd5eff0ac85a9f959d2c4538d049a
Dane Madsen <dane_madsen@hotmail.com>
2026-02-19 22:50:58 +1000

    tests

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -2,22 +2,28 @@ import { screen } from "expo-router/testing-library";
 
 import renderApp from "../__test_utilities__/render-app";
 
-describe("Maid Routes", () => {
+describe("Chat page", () => {
   beforeEach(() => renderApp());
 
-  it("Load Home Screen", async () => {
+  it("should render the prompt input", async () => {
     const input = await screen.findByTestId("prompt-input");
     expect(input).toBeOnTheScreen();
     expect(input.type).toBe("TextInput");
+  });
 
+  it("should render the send button", async () => {
     const sendButton = await screen.findByTestId("send-button");
     expect(sendButton).toBeOnTheScreen();
     expect(sendButton.type).toBe("View");
+  });
 
+  it("should render the open drawer button", async () => {
     const openDrawerButton = await screen.findByTestId("open-drawer-button");
     expect(openDrawerButton).toBeOnTheScreen();
     expect(openDrawerButton.type).toBe("View");
+  });
 
+  it("should render the menu button", async () => {
     const menuButton = await screen.findByTestId("menu-button");
     expect(menuButton).toBeOnTheScreen();
     expect(menuButton.type).toBe("View");
