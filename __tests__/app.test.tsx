c581d474dd743b72282ee146ff618de116238ba0
Dane Madsen <dane_madsen@hotmail.com>
2026-02-19 22:01:22 +1000

    test stuff

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -1,30 +1,9 @@
-import { renderRouter, screen } from "expo-router/testing-library";
+import { screen } from "expo-router/testing-library";
 
-import RootLayout from "../app/_layout";
-import About from "../app/about";
-import ChatLayout from "../app/chat/_layout";
-import Chat from "../app/chat/index";
-import Downloads from "../app/download";
-import Root from "../app/index";
-import Settings from "../app/settings";
+import renderApp from "../__test_utilities__/render-app";
 
 describe("Maid Routes", () => {
-  beforeEach(() => {
-    renderRouter(
-      {
-        "_layout": RootLayout,
-        "index": Root,
-        "chat/_layout": ChatLayout,
-        "chat/index": Chat,
-        "about": About,
-        "downloads": Downloads,
-        "settings": Settings,
-      },
-      {
-        initialUrl: "/",
-      }
-    );
-  });
+  beforeEach(() => renderApp());
 
   it("loads chat and shows the prompt input", async () => {
     const input = await screen.findByTestId("prompt-input");
